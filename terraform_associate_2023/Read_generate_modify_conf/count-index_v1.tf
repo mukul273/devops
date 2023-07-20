13fb4ce4ace2dacf4122eedb81a6c13b6b9d3102
mukul273 <msamak@gmail.com>
2023-07-20 17:16:50 -0400

    updated with conditional and README

---
+++ terraform_associate_2023/Read_generate_modify_conf/count-index_v1.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "elb_names" {
+  type = list
+}
+
+resource "aws_iam_user" "lb" {
+  name = var.elb_names[count.index]
+  count = 3
+  path = "/system/"
+}
